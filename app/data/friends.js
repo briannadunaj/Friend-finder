444121c4ad8a118270ba0b3fb30b707a4a9074b8
Brianna Dunaj <briannadunaj@me.com>
2017-06-28 01:37:46 -0400

    started survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,5 @@
+//add photos
+
 var friends = [{
 	name: "Michael",
 	numbers: [1, 3, 2, 4, 3, 2, 1],
--- server.js
+++ server.js
@@ -5,11 +5,40 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
+//html routes
 app.get("/", function (req, res){
 	console.log("getting home page", req.params.page)
 	res.sendFile(path.join(__dirname, "app/public/home.html"))
 });
 
+
+//api routes
+/*//search for specific friend
+app.get("/api/:friends?", function(req, res){
+	var chosen = req.params.friends;
+
+	if (chosen){
+		console.log(chosen);
+		for (var i = 0; i < friends.length; i++){
+			if (chosen === friends[i].name){
+				return res.json(friends)
+			}
+		}
+	}
+})*/
+
+// create new friend
+app.post("/api/new", function(req, res){
+	//req.body is equal to the json post sent from the user
+	var newfriend = req.body;
+	//add the json the user sent to the friends array
+	friends.push(newfriend);
+	//
+})
+
+
+
 app.listen(3000, function(){
-	console.log("Example app listening on port 3000!")
+	console.log("App listening on port 3000!")
 });
