5c277d349276b4841566124d705505c0b7d24ef7
Brianna Dunaj <briannadunaj@me.com>
2017-06-27 23:56:43 -0400

    setting up friends data and server file:

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [{
+	name: "Michael",
+	numbers: [1, 3, 2, 4, 3, 2, 1],
+	score: eval(numbers.join('+'));
+},
+{
+	name: "Gob",
+	numbers: [3, 2, 1, 4, 2, 3, 1],
+	score: eval(numbers.join('+'));
+},
+{
+	name: "George Michael",
+	numbers: [2, 3, 1, 3, 2, 1],
+	score: eval(numbers.join('+'));
+},
+{
+	name: "Lucille",
+	numbers: [1, 3, 2, 4, 3, 2],
+	score: eval(numbers.join('+'));
+},
+{
+	name: "Maybe",
+	numbers: [4, 3, 2, 1, 2, 3, 4],
+	score: eval(numbers.join('+'));
+},
+{
+	name: "Lindsey",
+	numbers: [3, 4, 2, 1, 3, 2],
+	score: eval(numbers.join('+'));
+}];
