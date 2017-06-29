254567c4113e50bfe09142acafb564900af7697d
Brianna Dunaj <briannadunaj@me.com>
2017-06-28 22:09:54 -0400

    added more questions, character answers, and character photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,44 @@
 //add photos
 
 var friends = [{
-	name: "Michael",
-	numbers: [1, 3, 2, 4, 3, 2, 1],
-	score: eval(numbers.join('+'));
+	"name": "Michael Bluth",
+	"photo": "https://brianlauritzen.files.wordpress.com/2013/05/michaelbluth.jpg",
+	"numbers": [5, 2, 4, 1, 5, 1, 1],
 },
 {
-	name: "Gob",
-	numbers: [3, 2, 1, 4, 2, 3, 1],
-	score: eval(numbers.join('+'));
+	"name": "Gob Bluth",
+	"photo": "http://www.billboard.com/files/media/arrested-development-gob-650-430.jpg",
+	"numbers": [1, 4, 2, 1, 1, 5, 4],
 },
 {
-	name: "George Michael",
-	numbers: [2, 3, 1, 3, 2, 1],
-	score: eval(numbers.join('+'));
+	"name": "George Michael Bluth",
+	"photo": "http://static.socialitelife.com/uploads/2013/07/26/george-michael-bluth-820x820.jpg",
+	"numbers": [3, 1, 3, 2, 1, 4, 1],
 },
 {
-	name: "Lucille",
-	numbers: [1, 3, 2, 4, 3, 2],
-	score: eval(numbers.join('+'));
+	"name": "Lucille Bluth",
+	"photo": "http://vignette2.wikia.nocookie.net/arresteddevelopment/images/5/5c/1x01_Pilot_%2823%29.png/revision/latest?cb=20120301045528",
+	"numbers": [4, 5, 3, 1, 5, 1, 5],
 },
 {
-	name: "Maybe",
-	numbers: [4, 3, 2, 1, 2, 3, 4],
-	score: eval(numbers.join('+'));
+	"name": "Maeby Fünke",
+	"photo": "http://3.bp.blogspot.com/-dBaiUv8K5gw/UQp6bjxtvaI/AAAAAAAAADY/cpxrcHnfk6c/s1600/maeby.jpg",
+	"numbers": [4, 3, 3, 2, 2, 5, 4],
 },
 {
-	name: "Lindsey",
-	numbers: [3, 4, 2, 1, 3, 2],
-	score: eval(numbers.join('+'));
+	"name": "Lindsay Bluth Fünke",
+	"photo": "https://vignette1.wikia.nocookie.net/arresteddevelopment/images/f/f4/Purple_-_Season_One_photoshoot_%288%29.jpeg/revision/latest?cb=20120429230807",
+	"numbers": [3, 5, 1, 1, 3, 3, 5],
+},
+{
+	"name": "Tobias Fünke",
+	"photo": "https://pbs.twimg.com/profile_images/644157694136422400/d4ho5P5S.jpg",
+	"numbers": [3, 4, 3, 1, 1, 4, 3],
+},
+{
+	"name": "Buster Bluth",
+	"photo": "https://vignette2.wikia.nocookie.net/arresteddevelopment/images/7/7b/Orange_-_Season_One_photoshoot_%281%29.jpeg/revision/latest/scale-to-width-down/300?cb=20120429230331",
+	"numbers": [1, 1, 5, 5, 1, 3, 3],
 }];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -5,11 +5,17 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var friends = require("./app/data/friends");
+var survey = require("./app/public/home.html")
 
 //html routes
 app.get("/", function (req, res){
 	console.log("getting home page", req.params.page)
 	res.sendFile(path.join(__dirname, "app/public/home.html"))
+});
+
+app.get("/survey", function(req, res){
+	res.sendFile(path.join(__dirname, "app/public/survey.html"))
 });
 
 
@@ -29,13 +35,15 @@ app.get("/api/:friends?", function(req, res){
 })*/
 
 // create new friend
-app.post("/api/new", function(req, res){
+app.post("/api/friends", function(req, res){
 	//req.body is equal to the json post sent from the user
-	var newfriend = req.body;
+	var newfriend = req.body.newFriend;
 	//add the json the user sent to the friends array
 	friends.push(newfriend);
 	//
-})
+});
+
+console.log(friends);
 
 
 
