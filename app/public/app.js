41cc5f9260f079913602c65d884f093f6af76c47
Brianna Dunaj <briannadunaj@me.com>
2017-06-30 01:21:23 -0400

    added function to calc user's most compatible friend

--- app/public/app.js
+++ app/public/app.js
@@ -1,29 +1,44 @@
-$(".submit-btn").on("click", function(){
-	console.log("hi");
-	userAnswers = [];
+$(document).ready(function(){
+	$(".submit-btn").on("click", function(){
+		console.log("hi");
+		userAnswers = [];
 
-	var q1 = $("#sel1").val();
-	userAnswers.push(q1);
-	var q2 = $("#sel2").val();
-	userAnswers.push(q2);
-	var q3 = $("#sel3").val();
-	userAnswers.push(q3);
-	var q4 = $("#sel4").val();
-	userAnswers.push(q4);
-	var q5 = $("#sel5").val();
-	userAnswers.push(q5);
-	var q6 = $("#sel6").val();
-	userAnswers.push(q6);
-	var q7 = $("#sel7").val();
-	userAnswers.push(q7);
+		var q1 = $("#sel1").val();
+		userAnswers.push(q1);
+		var q2 = $("#sel2").val();
+		userAnswers.push(q2);
+		var q3 = $("#sel3").val();
+		userAnswers.push(q3);
+		var q4 = $("#sel4").val();
+		userAnswers.push(q4);
+		var q5 = $("#sel5").val();
+		userAnswers.push(q5);
+		var q6 = $("#sel6").val();
+		userAnswers.push(q6);
+		var q7 = $("#sel7").val();
+		userAnswers.push(q7);
 
-	var newFriend = {
-		"name": $("#name").val().trim(),
-		"photo": $("#photo").val(),
-		"numbers": userAnswers
-	};
-	console.log(newFriend);
+		var newFriend = {
+			"name": $("#name").val().trim(),
+			"photo": $("#photo").val(),
+			"numbers": userAnswers
+		};
+		console.log(newFriend);
 
-	// add modal pop-up with result
+/*		$.post("/api/friends", newFriend, function(){
+			q1 = 0, q2 = 0, q3 = 0, q4 = 0, q5 = 0, q6 = 0, q7 = 0;
+		}).then(function(match){
+			var parsed = JSON.parse(match);
+			console.log(parsed);
+			
+		}); */
+		//console.log(parsed);
+		// add modal pop-up with result
+
+	});
+
+
 
 });
+
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,24 +11,54 @@ module.exports = function(app){
 	app.post("/api/friends", function(req, res){
 		friendsData.push(req.body);
 		res.json();
+
+		var closest = 40;
+		var userName = req.body.name;
+		var userPhoto = req.body.photo;
+		var userScores = req.body.numbers;
+		closestScore();
+
+		// determine the user's most compatible friend
+		function closestScore(){
+			var differencesArray = [];
+			var totalDifference = 0;
+			var closestNumber = 0;
+
+			// loop through all stored friends
+			for (var i = 0; i < friendsData.length; i++){
+				// loop through numbers array
+				for (var j = 0; j < userScores.length; j++){
+					// calculate the absolute value difference between the user's answers and each character's answers
+					// add up all of the differences
+					totalDifference += (Math.abs(userScores[j] - friendsData[i].numbers[i]));
+				} // end of userScores loop
+
+				// push each total difference to an array
+				differencesArray.push(totalDifference);
+				// reset sum
+				sum = 0;
+			} // end of friends loop
+
+			// loop through all the differences to find the lowest number (user's match)
+			for (var i = 0; i < differencesArray.length; i++){
+				// if the current number is less than a placeholder number
+				if (differencesArray[i] < closest){
+					// placeholder number becomes lowest difference number
+					closest = differencesArray[i];
+					// store the lowest difference
+					closestNumber = i;
+				}
+			} // end of differences array loop
+			console.log(closestNumber);
+			// store the compatible friend in variable
+			var match = friendsData[closestNumber];
+			// convert data to string
+			var string = JSON.stringify(match);
+			
+			res.end(string);
+			console.log(string);
+			//$(".modal-body").html(string);
+
+		}; // end of match function
 	});
-
-	//console.log(friendsData);
-
-
-	// code to compare numbers
-	// loop through friends array
-/*/	for (var i = 0; i < friendsData.length - 1; i++){
-		//console.log(friendsData[i].numbers);
-
-		//loop through each 'numbers' array 
-		for (var j = 0; j < friendsData[i].numbers.length; j++){
-			console.log(friendsData[i].numbers[j]);
-
-			difference += Math.abs(parseInt(req.body.numbers) - parseInt(friendsData[i].numbers[j]));
-			console.log(difference);
-		}
-	}
-/*/
-
 }
